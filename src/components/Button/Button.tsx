11b48ddcfcec8a9938e35b687383064cbe34c502
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-06 17:49:46 -0500

    feat: Create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import styles from './Button.module.css'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'flat' | 'primary'
+}
+
+export const Button = ({ variant = 'primary', children, ...props }: ButtonProps) => {
+  return (
+    <button
+      className={`${styles.btn} ${styles[variant]}`}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
