489cff27115231c9562c293aa5d1c6d76a73d824
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-06 17:50:24 -0500

    feat: Set styles css and add components to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,19 @@
+import { Button } from '../components/Button'
+import { InputField } from '../components/InputField'
+
+import styles from './HomeScreen.module.css'
+
 export const HomeScreen = () => {
   return (
-    <main>
-      <h1>Quizi</h1>
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Quizi</h1>
+        <p className={styles.description}>🙌 Saludos, ingresa el enlace a tu quiz para continuar.</p>
+      </header>
+      <div className={styles.wrapperActions}>
+        <InputField />
+        <Button>Cargar</Button>
+      </div>
     </main>
   )
 }
