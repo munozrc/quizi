2df5da3a3595cc0b6951fd5f093cd1a89d47f438
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-16 22:43:18 -0500

    feat: set new data and custom hook

--- src/screens/QuizScreen.tsx
+++ src/screens/QuizScreen.tsx
@@ -1,17 +1,34 @@
+import { useParams } from 'react-router-dom'
+
+import { useQuiz } from '../hooks/useQuiz'
+
 import styles from './QuizScreen.module.css'
 
 export const QuizScreen = () => {
+  const { id } = useParams()
+  const { quiz, question, checkAnswer } = useQuiz(id)
+
+  if (typeof quiz === 'undefined') return <p>Cargando...</p>
+  if (quiz === null) return <p>Quiz no encontrado</p>
+
   return (
     <main className={styles.container}>
       <section className={styles.quiz}>
         <header className={styles.quiz__hero}>
-          <p className={styles.quiz__question}>If David&apos;s age is 27 years old in 2011. What his age in 2003?</p>
+          <p className={styles.quiz__question}>{question?.statement}</p>
         </header>
         <footer className={styles.quiz__answers}>
-          <button className={styles.quiz__answer}>19 years</button>
-          <button className={styles.quiz__answer}>37 years</button>
-          <button className={styles.quiz__answer}>20 years</button>
-          <button className={styles.quiz__answer}>17 years</button>
+          {
+            question?.options.map((option) => (
+              <button
+                key={option.key}
+                className={`${styles.quiz__answer}`}
+                onClick={(e) => checkAnswer(e, option.key)}
+              >
+                {option.value}
+              </button>
+            ))
+          }
         </footer>
       </section>
     </main>
