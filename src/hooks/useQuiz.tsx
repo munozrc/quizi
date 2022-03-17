33a222a7e30d562a8941643961c54ee1942ac210
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-16 22:45:13 -0500

    feat: Create custom hook for manage quiz

---
+++ src/hooks/useQuiz.tsx
@@ -0,0 +1,33 @@
+import { MouseEvent, useEffect, useState } from 'react'
+
+import { Quiz } from '../typings'
+import demo from '../assets/demo.json'
+
+export const useQuiz = (id: string | undefined) => {
+  const [currentQuiz, setCurrentQuiz] = useState<Quiz | null | undefined >(undefined)
+  const [activeQuestion] = useState<number>(0)
+  const [isQuestionAnswered, setIsQuestionAnswered] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (id === 'demo') setCurrentQuiz(demo)
+    else setCurrentQuiz(null)
+  }, [id])
+
+  const checkAnswer = (event: MouseEvent<HTMLButtonElement>, key: number) => {
+    const { target } = event
+    const { classList } = target as HTMLButtonElement
+    const currentAnswer = currentQuiz?.questions[activeQuestion].answer
+
+    if (key === currentAnswer) classList.add('correct')
+    else classList.add('wrong')
+
+    setIsQuestionAnswered(true)
+  }
+
+  return {
+    quiz: currentQuiz,
+    question: currentQuiz?.questions[activeQuestion],
+    isQuestionAnswered,
+    checkAnswer
+  }
+}
