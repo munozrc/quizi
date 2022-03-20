787e7cd39831a30c233df94864686013a9801f21
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-19 21:23:32 -0500

    feat: Add first icon

---
+++ src/components/icons/ArrowIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from 'react'
+
+export const ArrowIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    stroke="currentColor"
+    fill="none"
+    strokeWidth={0}
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="m15.038 6.343-1.411 1.418 3.27 3.255-13.605.013.002 2 13.568-.013-3.215 3.23 1.417 1.41 5.644-5.67-5.67-5.643Z"
+      fill="currentColor"
+      stroke="none"
+    />
+  </svg>
+)
