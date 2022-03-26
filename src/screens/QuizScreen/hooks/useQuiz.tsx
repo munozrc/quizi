600559228d9abed6f644bdb8004c15c47fbdc7fa
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-26 13:12:16 -0500

    feat: Add persistence to values in quiz

--- src/screens/QuizScreen/hooks/useQuiz.tsx
+++ src/screens/QuizScreen/hooks/useQuiz.tsx
@@ -1,16 +1,24 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 
 import { QuizStatus } from '../../../typings'
 import demo from '../../../assets/demo.json'
 
 export const useQuiz = () => {
   const { id } = useParams()
+  const [searchParams, setSearchParams] = useSearchParams()
   const [currentQuiz, setCurrentQuiz] = useState<QuizStatus>(undefined)
   const [isStartedQuiz, setStartedQuiz] = useState<boolean>(false)
   const [activeQuestion, setActiveQuestion] = useState<number>(0)
 
   useEffect(() => {
+    const isStarted = searchParams.get('started') ?? 'false'
+    const currentQuestion = searchParams.get('currentIndex') ?? '0'
+
+    setSearchParams({ started: isStarted, currentIndex: currentQuestion })
+    setStartedQuiz(isStarted === 'true')
+    setActiveQuestion(+currentQuestion)
+
     if (id === 'demo') return setCurrentQuiz(demo)
     setCurrentQuiz(null)
   }, [id])
@@ -23,14 +31,23 @@ export const useQuiz = () => {
     const questionsNumber = currentQuiz.questions.length - 1
 
     // Check if there is a next question
-    if (activeQuestion < questionsNumber) setActiveQuestion(prev => prev + 1)
+    if (activeQuestion < questionsNumber) {
+      const value = activeQuestion + 1
+      setActiveQuestion(value)
+      setSearchParams({ started: 'true', currentIndex: value.toString() })
+    }
   }, [currentQuiz, activeQuestion])
+
+  const startQuiz = useCallback(() => {
+    setStartedQuiz(true)
+    setSearchParams({ started: 'true', currentIndex: activeQuestion.toString() })
+  }, [])
 
   return {
     quiz: currentQuiz,
     question: currentQuiz?.questions[activeQuestion],
     isStartedQuiz,
     nextQuestion,
-    startQuiz: () => setStartedQuiz(true)
+    startQuiz
   }
 }
