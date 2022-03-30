4ef0ac1e9d42a5d5b9ab50fd18aa50f2e8bb66bf
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-03-29 20:34:17 -0500

    feat: Change render and change props for RenderQuestion

--- src/screens/QuizScreen/QuizScreen.tsx
+++ src/screens/QuizScreen/QuizScreen.tsx
@@ -10,23 +10,22 @@ export const QuizScreen = () => {
 
   if (typeof quiz === 'undefined') return <Spinner />
   if (quiz === null) return <p>Quiz no encontrado</p>
-  if (typeof question === 'undefined') return <p>Error al cargar la pregunta</p>
 
   return (
     <PageLayout>
       <main className={styles.container}>
         <Setup
-          isVisible={quizStatus === 'making'}
+          isVisible={quizStatus === 'setup'}
           numberQuestions={quiz.questions.length}
           title={quiz.title}
           startQuiz={startQuiz}
         />
         <RenderQuestion
-          key={question.statement}
+          key={question?.statement}
           isVisible={quizStatus === 'started'}
           questions={quiz.questions}
           goToNextQuestion={nextQuestion}
-          {...question}
+          question={question}
         />
         <Results
           isVisible={quizStatus === 'finished'}
